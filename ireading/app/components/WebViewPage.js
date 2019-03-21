ea5468b9083c63ac45ce572daa0b84e3c5d68b27
wangzhenglian <1769032667@qq.com>
2019-03-21 17:38:10 +0800

    hide header

--- ireading/app/components/WebViewPage.js
+++ ireading/app/components/WebViewPage.js
@@ -2,6 +2,14 @@ import React, {Component} from 'react';
 import { View,WebView} from 'react-native';
 
 export default class WebViewPage extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  static navigationOptions ={
+    header:null
+  }
+
   render() {
     const {params} = this.props.navigation.state;
     console.log(params)
